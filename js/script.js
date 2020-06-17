146d3e80f0bb6e6033ff7ff04fe879fd2421408c
Ahmad Almosallam <abo7azm190@gmail.com>
2020-06-18 01:10:51 +0300

    done famous writers section

--- js/script.js
+++ js/script.js
@@ -6,4 +6,34 @@ $(document).ready(function() {
         } else
             $('.gallery').removeClass('change');
     });
+
+    $('.writers-accordion').click(function(event) {
+
+        if (event.target.id.split('-')[0] === 'button') {
+            $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
+            $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
+        }
+
+    });
+
+    var lastWidth = $(window).width();
+
+    $(window).resize(function() {
+        if ($(window).width() != lastWidth) {
+            //execute code here.
+            lastWidth = $(window).width();
+            if (lastWidth < 992) {
+                $('#book-div-1').addClass('order-0');
+                $('#book-div-2').addClass('order-1');
+                $('#writer').addClass('order-2');
+            } else {
+                $('#book-div-1').removeClass('order-0');
+                $('#book-div-2').removeClass('order-1');
+                $('#writer').removeClass('order-2');
+            }
+
+        }
+    })
+
+
 });
