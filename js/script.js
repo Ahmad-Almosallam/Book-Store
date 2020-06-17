6b81259becd38ab2ac299221fba380d21c9eea84
Ahmad Almosallam <abo7azm190@gmail.com>
2020-06-17 23:33:43 +0300

    end of header and gallery

--- js/script.js
+++ js/script.js
@@ -1,34 +1,9 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function() {
+    $(window).scroll(function() {
+        let pos = $(this).scrollTop();
+        if (pos >= 350) {
+            $('.gallery').addClass('change')
+        } else
+            $('.gallery').removeClass('change');
+    });
+});
